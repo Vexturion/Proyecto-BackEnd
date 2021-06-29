8618c06cd4b2b7cf577050c3051415e9bafad3b2
Vexturion <horizontedesucesos@outlook.com>
2021-06-28 23:58:44 -0500

    Servir archivos estaticos
    Definimos la carpeta donde guardaremos nuestra hoja de estilos, imagenes etc y le especificamos al servidor la ruta a los mismos con express.static

--- HTML/server.js
+++ HTML/server.js
@@ -1,12 +1,16 @@
-/* enviar HTML */
+/* Servir archivos estativos */
 const express = require ('express');
 
 const app = express();
 
-app.get('/', function(req, res){ /* definimos la ruta inicia; que va a responder con un documento html */
-    res.sendFile('index.html',{ /* nuestra funcion respondera a la peticion con un archivo html */
-        root: __dirname         /* utilizamos root: ___dirname para decirle al servidor donde esta el archivo. 
-        Con ___dirname/express no tenemos que establecer la ruta absoluta y somos libres de mover el proyecto a donde queramos sin tener que modificar el codigo*/
+/* middleware de archivos estaticos */
+/* especificamos una carpeta donde tendremos los arcvhivos estaticos con express.static */
+app.use('/assets', express.static('assets')); /* colocamos la direccion de los archivos estaticos detras de una ruta, que no tiene que ver con el nombre de la carpeta
+donde si tiene que ver el nombre de la carpeta es en el argumento para el metodo express.static('assets') */
+
+app.get('/', function(req, res){ 
+    res.sendFile('index.html',{ 
+        root: __dirname         
     });
 });
 
