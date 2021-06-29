aafd92d5cf5c2b431a0fcf8c6b891cb59c88ef27
Vexturion <horizontedesucesos@outlook.com>
2021-06-28 20:10:08 -0500

    Datos con Post
    Modificamos nuestro codigo para agregarle un metodo para recibir datos con POST

--- express.js
+++ express.js
@@ -1,13 +1,26 @@
-/* la direccion a la cual mandamos la peticion: http://localhost3000/saludo */
-/* la peticion con nuestro dato .name , con query params (incluyendo los datos en la direccion) 
-separando de la direccion ?
-http://localhost3000/saludo?name=Alejandro%20Cruz (%20 para espacios) */
-const express = require ('express');
+/* Recibimos datos con Post 
+la principal diferencia de POST y GET es que la informacion se envia en el CUERPO de la peticion y no en
+la peticion y no en la direccion*/
+
+const express = require('express');
+const bodyParser = require('body-parser'); /* importamos nuestra libreria body-parser, para poder analizar el cuerpo 
+de la peticion ya que antes de tenerla disponible en codigo el cuerpo de la peticion podria venir con informacion JSON
+o datos estructurados en otro formato. Parse es necesario para poder ver la peticion de forma uniforme y clara */
+
+
 const app = express();
-app.get('/saludo',function(req,res){  /* Cambiamos la direccion de '/' a '/saludo' */
-    res.send(`Hola ${req.query.name}`); /* Usamos template strings, ya que soporta interpolacion con $. 
-    Dentro de nuestro objeto req tenemos otro objeto de nombre query que tiene una propiedad por cada dato que recibe en la peiticion.
-    en este caso la propiedad .name */
+
+/* en esta linea establecemos que como parte del proceso use el analizador body parser para leer el cuerpo de datos de una peticion,
+particularmente le pedimos que use el analizador para leer el cuerpo de una peticion que venga con el formqto url encoded
+extended es necesario para enviar enviar parametros anidados y asi enviar un objeto con propiedades en la peticion */
+app.use(bodyParser.urlencoded({ extended: true })) 
+
+app.get('/saludo', function (req, res) {
+    res.send(`Hola ${req.query.name}`);
+})
+
+app.post('/', function (req, res) {  /* establecemos nuestro metodo post y la direccion a donde se enviara, en este caso "/"*/
+    res.send(`Hola ${req.body.name}`); /* aqui colocamos que nuestra peticion va en la propiedad cuerpo, ya que es un post los datos no se recibiran en query params */
 })
 
 app.listen(3000);
