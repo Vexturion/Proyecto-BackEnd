890b99a800213a9310950f676cd9628d59cc69c0
Vexturion <horizontedesucesos@outlook.com>
2021-06-30 22:32:10 -0500

    Cache de archivos estaticos
    Aprendemos que es un Etag y por que el uso de maxAge es mas recomendable para la eficiencia de nuestros proyectos web

--- HTML/server.js
+++ HTML/server.js
@@ -1,18 +1,18 @@
-/* Motor de vistas */
+/* cache de archivos estaticos 
+etag es una representacion del contenido del archivo
+que usa el navegador almacena para comparar peticiones*/
 const express = require ('express');
 
 const app = express();
 
-/* Usamos el metodo set del objeto app para configurar nuestro motor de vistas; en el primer argumento indicamos que usamos un motor de vistas
-y en el segundo el motor de vistas que usaremos, en este caso ejs. Por defecto Express usara la carpeta views que creamos en nuestro proyecto 
-para almacenar los archivos de las vistas y la extension de los archivos dependera del motor de vusta que usemos, en este caso .ejs */
 app.set('view engine', 'ejs');
 
-app.use('/assets', express.static('assets')); 
+app.use('/assets', express.static('assets',{
+etag: false /* dado que el etag es menos eficiente, usamos ETAG false para desactivarlo ya que por defecto nuestro servidor va a usarlos */
+maxAge: 180000})); /* en su lugar utilizaremos maxAGE, para establecer el tiempo en que la respuesta a x peticion no va a cambiar en una n cantidad de tiempo  */
 
 app.get('/', function(req, res){ 
-    res.render('index');          /*res.sendFile('index.html',{   con motores de vista no es necesario especificar rutas de archivo, unicamente usamos el metodo .render
-                                        root: __dirname */         
+    res.render('index');              
 });
 
 app.listen(3000);
