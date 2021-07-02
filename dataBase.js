87176558894fa2e2abfd47581f4ddde4de37980d
Vexturion <horizontedesucesos@outlook.com>
2021-07-02 12:42:24 -0500

    Insertar registro en una base de datos
    Despues de crear nuestra base de datos insertamos un registro con una peticion POST

--- dataBase.js
+++ dataBase.js
@@ -1,15 +1,28 @@
-/* primera comsulta (creamos una tabla)
-normalmente al hacer consultas con lenguajes de programacion 
-deberemos de abrir la conexion y luego cerrarla, con sqlite3 no es
-necesario mandar a llamar un metodo para abrir una conexion. Al crear un objeto
-de conexion con la base de datos la conexion se abre automaticamente  */
-const sqlite3 = require('sqlite3'); /* importamos nuestra libreria */
+/* insertar registros */
+const express =  require('express'); /* importamos express, pues vamos a crear un servidor */
+const sqlite3 = require('sqlite3'); /* importamos sqlite3 pues vamos a modificar nuestra base de datos */
+const bodyParser = require('body-parser'); /* dado que vamos a enviar una peticion con POST, es necesario que importemos bodyparser */
 
-/* aqui creamos el objeto de conexion mencionado anteriormente, colocando en el argumento
-el nombre de nuestra base de datos */
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true})); /* configuramos bodyparser */
+
 let db = new sqlite3.Database('proyecto-backend');
 
-/* despues, procedemos a realizar una consulta en lenguaje SQL (en este caso vamos a crear una tabla)*/
-db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))');
-/* al final, cerramos la conexion para evitar fugas de memoria */
-db.close();
+app.post('/pendientes', function(req,res){ /* configuramos la insercion al evento de una peticion post a la direccion pendientes */
+    db.run("INSERT INTO tasks(description) VALUES('hola mundo')"); /* la estructura es la misma db.run y las instrucciones SQL como argumentos */
+    res.send('Insercion finalizada exitosamente'); /* al final, como respuesta mostraremos que la insercion se realizo exitosamente */
+})
+
+/* db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))'); ya que vamos a insertar registros en la tabla tasks no es necesario
+volver a ejecutar la instruccion para crear la tabla, ademas no podemos crear una tabla ya creada */
+
+app.listen(3000);
+
+/* para poder recibir peticiones colocaremos un listener a un evento del proceso */
+process.on('SIGINT',function(){ /* dicho evento sera SIGINT */
+    console.log('Adios - atte el servidor');
+    db.close(); /* no colocaremos el cierre de la conexion con terminal al final ya que la conexion se cerraria antes de que seamos capaces de enviarle peticiones */
+    process.exit(); /* esta instruccion nos permite cerrar el servidor de node independientemente de donde se ejecute */
+
+})
