070223a76d26809b8ffe035f18b83b2ba837b612
Vexturion <horizontedesucesos@outlook.com>
2021-07-06 19:28:38 -0500

    Sanitize
    Aprendemos a usar Sanitize para evitar un ataque de inyeccion SQL

--- dataBase.js
+++ dataBase.js
@@ -1,28 +1,33 @@
-/* insertar registros */
-const express =  require('express'); /* importamos express, pues vamos a crear un servidor */
-const sqlite3 = require('sqlite3'); /* importamos sqlite3 pues vamos a modificar nuestra base de datos */
-const bodyParser = require('body-parser'); /* dado que vamos a enviar una peticion con POST, es necesario que importemos bodyparser */
+/* Sanitize */
+const express =  require('express'); 
+const sqlite3 = require('sqlite3'); 
+const bodyParser = require('body-parser'); 
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true})); /* configuramos bodyparser */
+app.use(bodyParser.urlencoded({extended: true})); 
 
 let db = new sqlite3.Database('proyecto-backend');
 
-app.post('/pendientes', function(req,res){ /* configuramos la insercion al evento de una peticion post a la direccion pendientes */
-    db.run("INSERT INTO tasks(description) VALUES('hola mundo')"); /* la estructura es la misma db.run y las instrucciones SQL como argumentos */
-    res.send('Insercion finalizada exitosamente'); /* al final, como respuesta mostraremos que la insercion se realizo exitosamente */
+app.post('/pendientes', function(req,res){ 
+    /* db.run("INSERT INTO tasks(description) VALUES('hola mundo')") dado que este hola mundo es estatico, cambiaremos nuestro codigo para insertar elementos que
+    vengan en el cuerpoo de la peticion http de la siguiente manera */
+    /*db.run(`INSERT INTO tasks(description) VALUES('${req.body.description}')`); /* sin embargo de esta forma exponemos nuestra base de datos a un ataque SQL injection  */
+    /* utilizaremos sanitize para evitar que lo que el usuario envia se ejecute como comandos de SQL
+    para usar SANITIZE en SQLite3 lo haremos de la siguienre manera */
+    db.run(`INSERT INTO tasks(description) VALUES(?)`, req.body.description);
+    /* esto evita que lo que introduce el cliente sea leido como instrucciones SQL y se lea como un string normal
+    en el parentesis de VALUES colocaremos ? y despues de la instruccion SQL los parametros que van a reemplazar a ? 
+    sanitize va a limpiar lo que envie el usuario antes de insertarlo */
+    res.send('Insercion finalizada exitosamente'); 
 })
-
-/* db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))'); ya que vamos a insertar registros en la tabla tasks no es necesario
-volver a ejecutar la instruccion para crear la tabla, ademas no podemos crear una tabla ya creada */
 
 app.listen(3000);
 
-/* para poder recibir peticiones colocaremos un listener a un evento del proceso */
-process.on('SIGINT',function(){ /* dicho evento sera SIGINT */
+
+process.on('SIGINT',function(){ 
     console.log('Adios - atte el servidor');
-    db.close(); /* no colocaremos el cierre de la conexion con terminal al final ya que la conexion se cerraria antes de que seamos capaces de enviarle peticiones */
-    process.exit(); /* esta instruccion nos permite cerrar el servidor de node independientemente de donde se ejecute */
+    db.close(); 
+    process.exit(); 
 
 })
